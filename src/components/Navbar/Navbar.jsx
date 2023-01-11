4a5fdb0f485d51a0c627240f8097a85ee9f00126
PoojaD12345 <dhuripooja01@gmail.com>
2023-01-11 15:42:04 +0530

    creating navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import style from './Navbar.module.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className={style.Navbar_main_div}>
+
+        {/* logo icon */}
+        <div>
+            <Link to="/">
+            <img src="https://www.dogbreedinfo.com/images32/DBICLogoDogBreedInfoCenter3.PNG" alt="logo" /></Link>
+        </div>
+
+        {/* list of breed and detail of the breed */}
+        {/* <div className={style.Navbar_breed_div}>
+                <Link to="/detail" className={style.Navbar_detail}>Detail Of Breed</Link>
+        </div> */}
+
+        {/* logout button */}
+        <div>
+            <button>Logout</button>
+        </div>
+    </div>
   )
 }
 
