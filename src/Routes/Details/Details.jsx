2fc7ed4ad4dea82a49df3c6cfa9c8e1d519a63d2
PoojaD12345 <dhuripooja01@gmail.com>
2023-01-19 23:23:39 +0530

    made changes in details page

--- src/Routes/Details/Details.jsx
+++ src/Routes/Details/Details.jsx
@@ -6,12 +6,14 @@ import style from './Details.module.css'
 const Details = () => {
   
     const [random,setRandom]=useState();
+    const [loading,setLoading]=useState(true)
 
     const getImage=async()=>{
         await axios.get("https://dog.ceo/api/breeds/image/random")
         .then((res)=>{
             //  console.log(res)
              setRandom(res.data.message)
+             setLoading(false)
         })
     }
     useEffect(() => {
@@ -21,7 +23,9 @@ const Details = () => {
 
   return (
     <div className={style.random_image_div}>
-        <img src={random} alt="random Image"className={style.random_image} />
+        {
+          loading ? <img src="https://raw.githubusercontent.com/Codelessly/FlutterLoadingGIFs/master/packages/cupertino_activity_indicator_square_medium.gif" alt="gif" /> : <img src={random} alt="random Image"className={style.random_image} />
+        }
     </div>
   )
 }
